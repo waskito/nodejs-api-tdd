570272cc0e722e66b8ff8139ddc4dfd568741f1f
Ahmad Rosid <alahmadrosid@gmail.com>
2019-10-15 15:45:22 +0700

    feat: user endpoint api

---
+++ README.md
@@ -0,0 +1,1 @@
+# Demo NodeJS REST Api
---
+++ test/index.test.js
@@ -0,0 +1,25 @@
+var expect = require('chai').expect;
+var request = require('supertest');
+var server = require('../src/app');
+
+describe('User API Tests', () => {
+    it('POST /users/ create new user', async () => {
+        const response = await request(server).post('/users').send({
+            name: 'Ahmad rosid',
+            email: 'ahmadrosid@gmail.com',
+            password: '123456'
+        });
+        expect(200).to.equal(200)
+        console.log(response.body);
+        expect(response.body).to.be.an.instanceof(Object);
+        expect(response.body).to.include.keys(['token', 'user']);
+    })
+})
+
+
+//run once after all tests
+after(function (done) {
+    var mongoose = require('mongoose');
+    console.log('Deleting test database');
+    mongoose.connection.db.dropDatabase(done);
+});
